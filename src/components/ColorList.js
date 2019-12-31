d1a28d95c8aef175841ccb57ffae423e2d0dcdf3
John Curry <fastpenguin91@gmail.com>
2019-12-31 13:41:45 -0800

    updated pagination

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -5,36 +5,16 @@ import gql from "graphql-tag";
 //import { BrowserRouter as Link, useRouteMatch } from "react-router-dom";
 import { BrowserRouter as Router, Link, useRouteMatch } from "react-router-dom";
 
-function GetPreviousPage(currentPage) {
-  if (currentPage != 0) {
-    console.log("current page");
-    console.log(currentPage);
-    return (
-      <Link to={"/colors/" + (parseInt(currentPage) - 1)}>
-        <span style={{ margin: "10px" }}>previous</span>
-      </Link>
-    );
-  }
-}
 
-function GetNextPage(currentPage, listLength) {
-  if (listLength >= 5) {
-    return (
-      <Link to={"/colors/" + (parseInt(currentPage) + 1)}>
-        <span style={{ margin: "10px" }}>next</span>
-      </Link>
-    );
-  }
-}
 
 function ColorList() {
   let match = useRouteMatch();
 
-  GetPreviousPage(match);
-
   let skipNum = 12 * match.params.page;
   const COLORS_QUERY = gql`query { colors( first: 12 skip: ${skipNum}) { id color_code } }`;
+  const COUNT_COLORS_QUERY = gql`query { colors { id color_code } }`;
   return (
+    <div>
     <Query query={COLORS_QUERY}>
       {({ loading, error, data }) => {
         if (loading) {
@@ -45,12 +25,7 @@ function ColorList() {
           console.log(error);
           return <div>Error: </div>;
         }
-        console.log("data");
-        console.log(data);
         const colorsToRender = data.colors;
-        console.log("arrays...");
-        console.log(data.colors.slice(0, 3));
-        console.log(data.colors.slice(0, 3));
 
         return (
           <div>
@@ -80,9 +55,11 @@ function ColorList() {
                           bottom: "0",
                           margin: "0",
                           paddingBottom: "10px",
+                          paddingTop: "10px",
                           textAlign: "center",
                           width: "100%",
-                          borderRadius: "0px 0px 10px 10px"
+                          borderRadius: "0px 0px 10px 10px",
+                          fontSize: "24px"
                         }}
                       >
                         {color.color_code}
@@ -92,13 +69,47 @@ function ColorList() {
                 </Link>
               ))}
 
-              {GetPreviousPage(match.params.page)}
-              {GetNextPage(match.params.page, colorsToRender.length)}
             </div>
           </div>
         );
       }}
     </Query>
+
+
+    <Query query={COUNT_COLORS_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <div>Fetching</div>;
+        }
+        if (error) {
+          console.log("error");
+          console.log(error);
+          return <div>Error: </div>;
+        }
+        const colorsToRender = data.colors;
+        const numOfPages = Math.ceil(data.colors.length / 12);
+        let numOfPagesArr = [];
+        for (let i=0; i < numOfPages; i++) {
+          numOfPagesArr.push(i);
+        }
+
+        return (
+          <div>
+            <div style={{ marginLeft: "17%"}}>
+              {numOfPagesArr.map(page => (
+                <Link to={"/colors/" + page}>
+                  <span style={{ margin: "10px" }}>{page}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
+        );
+      }}
+    </Query>
+
+
+
+    </div>
   );
 }
 
